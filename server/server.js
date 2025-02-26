3b2bbae2c425250ccf31dad282d8329b111baee9
gourishchouhan <gourishchouhan338@gmail.com>
2025-02-26 09:39:29 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express()
+const port = 5000
+
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send("Server is running")
+});
+app.listen(port,()=>{
+    console.log(`Server is running at port ${port}`)
+});
+app.post('/signup', (res,req) => {
+    const {username, email, password, dateOfBirth} = req.body;
+    if(!username) {
+        return res.status(400).json({error:"Username cannot be empty"})
+    }
+    if(!email) {
+        return res.status(400).json({error:"Email cannot be empty"})
+    }
+    if(!password) {
+        return res.status(400).json({error:"Password cannot be empty"})
+    }
+    if (password.length > 8 && password.length<=16){
+        return res.status(400).json({error:"Password length should be greater than 8 or less than equal to 16"})
+    }
+    
+    res.status(201).json({
+        message:"User created successfuly",
+        user: {
+            username,
+            email,
+            dateOfBirth
+        }
+    })
+})
